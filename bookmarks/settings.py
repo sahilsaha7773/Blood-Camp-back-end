2f0c823b6ed09c234f7ec0b7ef51da8c836e1af2
Sahil Saha <35343652+sahilsaha7773@users.noreply.github.com>
2020-06-30 19:13:53 +0530

    Added STATIC_ROOT

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -127,6 +127,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-# Activate Django-Heroku.
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
