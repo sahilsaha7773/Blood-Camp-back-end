459a1cbbed0e5475cdc137a3ce7e48b43ffea518
Sahil Saha <35343652+sahilsaha7773@users.noreply.github.com>
2020-07-02 11:10:45 +0530

    Added database for heroku

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -84,11 +84,12 @@ WSGI_APPLICATION = 'bookmarks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
